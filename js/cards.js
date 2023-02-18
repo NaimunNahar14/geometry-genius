7f3095f9646f6f0ef99e2fb22319358f66f15875
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-02-18 03:15:45 -0800

    try to responsive

--- js/cards.js
+++ js/cards.js
@@ -143,20 +143,20 @@ function displayData(nameOfC, resultOfC) {
   function disabledButton(id) {
     document.getElementById(id).setAttribute("disabled", true);
   }
-  function randomColor() {
-    let color = [];
-    for (let i = 0; i < 3; i++) {
-      color.push(Math.floor(Math.random() * 256));
-    }
-    return 'rgb(' + color.join(', ') + ')';
-  } 
+  // function randomColor() {
+  //   let color = [];
+  //   for (let i = 0; i < 3; i++) {
+  //     color.push(Math.floor(Math.random() * 256));
+  //   }
+  //   return 'rgb(' + color.join(', ') + ')';
+  // } 
   
-  document.addEventListener("mouseover", function(){
-    document.body.style.backgroundColor = randomColor();
-  });
+  // document.addEventListener("mouseover", function(){
+  //   document.body.style.backgroundColor = randomColor();
+  // });
   
-  document.querySelector('circle').addEventListener('mouseover', event => {
-        event.target.style.fill = randomColor();
-  });
+  // document.querySelector('circle').addEventListener('mouseover', event => {
+  //       event.target.style.fill = randomColor();
+  // });
 
   
